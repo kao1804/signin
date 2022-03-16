c2b146138ca3267973c76ad5af10576a70757265
Your Name <you@example.com>
2022-03-16 10:07:42 +0700

    initial commit

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MessageService } from '../message.service';
 import { CookieService } from 'ngx-cookie-service';
-import { FormControl, FormControlName, FormGroup ,  Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  @Input() formGroup: any;
+  @Input() require: boolean = false;
+  @Input() controlName: any;
+  @Input() title: string = '';
+  @Input() placeHolder: string = '';
+  @Input() type: string = '';
+  @Input() id: string = '';  
+  constructor(private router: Router) {}
 
-  @Input() formGroup:any;
-  @Input() require : boolean = false;
-  @Input() name:any;
-  @Input() title:string ="";
-  @Input() placeHolder: string = "";
-  @Input() type:string ="";
-  @Input() id:string ="";
-
-  constructor(private router: Router) { }
-  
-
-  ngOnInit(): void {
-  } 
-  
-
+  ngOnInit(): void {}
 }
